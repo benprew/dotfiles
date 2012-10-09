b6b97f87e09aa2e661a85e2192128147d546a20a
Ben Prew <ben.prew@gmail.com>
2012-10-09 09:30:18 -0700

    remove ledger configs

--- ledger/init.el
+++
@@ -1,7 +0,0 @@
-(require-el-get
- '(:name ledger
-         :type http
-         :url "https://raw.github.com/jwiegley/ledger/next/lisp/ledger.el"))
-
-(post-init (lambda ()
-    (setenv "LEDGER_FILE" "~/financials/ledger.txt")))
