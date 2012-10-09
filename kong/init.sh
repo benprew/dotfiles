13d145f333308e99f4633de3c7385f2e1c180f2c
Ben Prew <ben.prew@gmail.com>
2012-10-09 11:56:09 -0700

    use per-repository configs instead of global environment config

--- kong/init.sh
+++ kong/init.sh
@@ -1,6 +1,3 @@
-export EMAIL='bprew@kongregate.com'
-export GIT_COMMITTER_EMAIL=$EMAIL
-export GIT_AUTHOR_EMAIL=$EMAIL
 export KONGROOT="/Users/benprew/src/kongregate"
 
 if [[ -f $HOME/.kongpass ]]; then
