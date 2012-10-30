6b0d470c3ae31c73ad5016762782631058c0e7f5
Ben Prew <ben.prew@gmail.com>
2012-10-30 15:16:27 -0700

    el-get wants progn instead of lambda

--- puppet/init.el
+++ puppet/init.el
@@ -2,6 +2,6 @@
        :description "A simple mode for editing puppet manifests"
        :type http
        :url "https://raw.github.com/puppetlabs/puppet/master/ext/emacs/puppet-mode.el"
-       :after (lambda ()
+       :after (progn
                 (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests" t)
                 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode)))))
