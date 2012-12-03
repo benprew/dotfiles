a3db1a7065f1437389aa932f46877504b8f371d1
Ben Prew <ben.prew@gmail.com>
2012-12-03 11:00:09 -0800

    kong fixes

--- kong/init.sh
+++ kong/init.sh
@@ -3,3 +3,5 @@ export KONGROOT="/Users/benprew/src/kongregate"
 if [[ -f $HOME/.kongpass ]]; then
     . ~/.kongpass
 fi
+
+alias ku='ssh kongdev sudo /sbin/service unicorn stop; sleep 2; ssh kongdev sudo /sbin/service unicorn start'
