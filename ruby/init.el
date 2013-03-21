ed1cecb620698910b1d27b14ed07cba97110b2a7
Ben Prew <ben.prew@gmail.com>
2013-03-21 06:56:43 -0700

    ruby-mode for puppet files

--- ruby/init.el
+++ ruby/init.el
@@ -15,6 +15,7 @@
   (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.pp\\'" . ruby-mode))
   (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
   (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
   (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
