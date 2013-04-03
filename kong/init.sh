3350a18d61b06f3dd22c07a665ec5fe5d3cc054a
Ben Prew <ben.prew@gmail.com>
2013-04-02 21:13:13 -0700

    Set path for ruby version

--- kong/init.sh
+++ kong/init.sh
@@ -6,3 +6,6 @@ fi
 
 alias ku='ssh kongdev sudo /sbin/service unicorn stop; sleep 2; ssh kongdev sudo /sbin/service unicorn start'
 alias s="open-story"
+
+export PATH="$PATH:/usr/local/Cellar/ruby/1.9.3-p194/bin"
+
