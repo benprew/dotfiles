8f731663887b38afcbbafe4373c182ce85d8b310
Ben Prew <ben.prew@gmail.com>
2013-04-25 14:36:16 -0700

    slightly longer sleep between unicorn restart

--- kong/init.sh
+++ kong/init.sh
@@ -4,7 +4,7 @@ if [[ -f $HOME/.kongpass ]]; then
     . ~/.kongpass
 fi
 
-alias ku='ssh kongdev sudo /sbin/service unicorn stop; sleep 2; ssh kongdev sudo /sbin/service unicorn start'
+alias ku='ssh kongdev sudo /sbin/service unicorn stop; sleep 5; ssh kongdev sudo /sbin/service unicorn start'
 alias s="open-story"
 
 export PATH="$PATH:/usr/local/Cellar/ruby/1.9.3-p194/bin"
