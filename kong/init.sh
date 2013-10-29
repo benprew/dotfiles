66095002816132f508ee892dbcedacfc8f8ed851
Ben Prew <ben.prew@gmail.com>
2013-10-29 14:38:29 -0700

    upgrade version of ruby

--- kong/init.sh
+++ kong/init.sh
@@ -7,5 +7,4 @@ fi
 alias ku='ssh kongdev sudo /sbin/service unicorn stop; sleep 5; ssh kongdev sudo /sbin/service unicorn start'
 alias s="open-story"
 
-export PATH="$PATH:/usr/local/Cellar/ruby/2.0.0-p195/bin"
-
+export PATH="$PATH:/usr/local/Cellar/ruby/2.0.0-p247/bin"
