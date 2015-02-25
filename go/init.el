7949a4a71c2549d35ab4187f0a0fac07f82e951a
Ben Prew <ben.prew@gmail.com>
2015-02-24 21:10:07 -0800

    gofmt go buffers before saving them

--- go/init.el
+++ go/init.el
@@ -1,5 +1,12 @@
 (require-el-get 'go-mode)
 
-(post-init (lambda()
-  (add-to-list 'interpreter-mode-alist
-               '("gorun" . go-mode))))
+(post-init
+ (lambda()
+   (add-hook 'before-save-hook
+             (lambda()
+               (if (derived-mode-p 'go-mode)
+                   (gofmt))))
+
+   (add-hook 'go-mode-hook 'gofmt-on-save)
+   (add-to-list 'interpreter-mode-alist
+                '("gorun" . go-mode))))
