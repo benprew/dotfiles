1eb28a6bb5eb7426cfb20180b5a4ea8cc2aebc54
Ben Prew <ben@throwingbones.com>
2016-12-20 15:10:38 -0800

    emacs prelude files

--- core/emacs.d/personal/init.el
+++ core/emacs.d/personal/init.el
@@ -1,2 +1,16 @@
-(setq prelude-theme 'solarized-dark)
-(set-face-font 'default "Source Code Pro 14")
+(prelude-require-packages '(solarized-theme dash-at-point web-mode))
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+
+(require 'prelude-helm-everywhere)
+(require 'prelude-org)
+(require 'dash-at-point)
+
+(add-hook 'prog-mode-hook 'prelude-enable-whitespace)
+
+(setq global-flycheck-mode t)
+
+(windmove-default-keybindings 'super)
+
+(setq initial-scratch-message ";Don't ignore your dreams\n;Don't work too much\n;Say what you think\n;Cultivate friendships\n;Be happy.\n\n")
---
+++ core/emacs.d/personal/preload/init.el
@@ -0,0 +1,2 @@
+(setq prelude-theme 'solarized-dark)
+(set-face-font 'default "Source Code Pro 14")
