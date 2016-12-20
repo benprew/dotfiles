c6d02086a2ca929d1b8b1a20801e919586aedb37
Ben Prew <ben@throwingbones.com>
2016-12-20 13:36:40 -0800

    emacs prelude customizations

---
+++ core/emacs.d/personal/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-flycheck-mode t)
+ '(package-selected-packages
+   (quote
+    (slim-mode zop-to-char zenburn-theme yaml-mode which-key web-mode volatile-highlights vkill undo-tree solarized-theme smartrep smartparens smart-mode-line scss-mode puppet-mode ov operate-on-number move-text markdown-mode magit json-mode imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck fish-mode expand-region exec-path-from-shell easy-kill discover-my-major diminish diff-hl dash-at-point csv-mode crux color-theme-solarized browse-kill-ring beacon anzu ace-window)))
+ '(prelude-clean-whitespace-on-save t)
+ '(prelude-whitespace t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
