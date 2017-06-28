0c882fe1e3fdf78e60019ee4d1d2d8bd70e46764
Ben Prew <ben@throwingbones.com>
2017-06-27 20:22:23 -0700

    go changes

--- go/init.el
+++ go/init.el
@@ -2,11 +2,21 @@
 
 (post-init
  (lambda()
-   (add-hook 'before-save-hook
-             (lambda()
-               (if (derived-mode-p 'go-mode)
-                   (gofmt))))
+   ;; Company Mode Notes
+   ;; http://company-mode.github.io/
+   ;; Once installed, enable company-mode with M-x company-mode.
 
-   (add-hook 'go-mode-hook 'gofmt-on-save)
+   ;; Completion will start automatically after you type a few
+   ;; letters. Use M-n and M-p to select, <return> to complete or <tab>
+   ;; to complete the common part. Search through the completions with
+   ;; C-s, C-r and C-o. Press M-(digit) to quickly complete with one of
+   ;; the first 10 candidates.
+
+   ;; Type M-x company-complete to initiate completion manually. Bind
+   ;; this command to a key combination of your choice.
+
+   (add-hook 'go-mode-hook 'go-eldoc-setup)
+
+   (add-hook 'before-save-hook 'gofmt-before-save)
    (add-to-list 'interpreter-mode-alist
                 '("gorun" . go-mode))))
