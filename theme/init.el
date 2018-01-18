f0fbda436bc4619fa339744decf4e5fae0e2135d
Ben Prew <ben@throwingbones.com>
2018-01-17 23:33:37 -0800

    set theme in prelude not in theme/init.el

--- theme/init.el
+++ theme/init.el
@@ -1,14 +1,1 @@
 (prelude-require-packages '(color-theme-solarized))
-
-(defun night ()
-  (interactive)
-  (color-theme-solarized-dark))
-
-(defun day ()
-  (interactive)
-  (color-theme-solarized-light))
-
-(defun theme-init ()
-  (day))
-
-(theme-init)
