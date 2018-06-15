f44c82aeb480ca351d486235730fbcf75fcb4468
Ben Prew <ben@throwingbones.com>
2018-06-14 22:48:43 -0700

    remove api key from repo

--- kong/init.fish
+++ kong/init.fish
@@ -1,8 +1,6 @@
 set -x PATH $PATH /usr/local/lib/ruby/gems/2.1.0/bin
 set -x PATH $PATH /Applications/SnowSQL.app/Contents/MacOS/
 
-set -x HOMEBREW_GITHUB_API_TOKEN 8ea2c7459f0e5e60f322590925a7f6d6037d674f
-
 alias gs 'git status'
 alias gd 'git diff'
 alias dc 'docker-compose'
