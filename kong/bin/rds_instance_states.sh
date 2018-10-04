fd1c02e091f4e685212cb22b9571b365babfb44d
Ben Prew <ben@throwingbones.com>
2018-10-03 20:55:17 -0700

    Update tracker to use v5 and add a "todo" command, add rds status script

---
+++ kong/bin/rds_instance_states.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~/src/pints/master
+
+STORY_IDS=$(aws rds describe-db-instances --query DBInstances[].DBInstanceIdentifier | egrep -po '[0-9]{8,}')
+for s in $STORY_IDS; do
+  STATE=$(tracker "$s" story | jq -r '.current_state')
+  if [[ $STATE = 'accepted' ]]; then
+    BRANCH=$(git ls-remote --heads 2>&1 |grep "$s")
+    echo "$STATE $s $BRANCH";
+  else
+    echo "$STATE $s"
+  fi
+done
