3b184ba95ec5702375e616bd807e1fef5cc136b9
Ben Prew <ben@throwingbones.com>
2018-11-20 14:04:15 -0800

    add kong changes

--- kong/bin/rds_instance_states.sh
+++ kong/bin/rds_instance_states.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd ~/src/pints/master
+cd ~/src/pints/master || exit 1
 
 STORY_IDS=$(aws rds describe-db-instances --query DBInstances[].DBInstanceIdentifier | egrep -po '[0-9]{8,}')
 for s in $STORY_IDS; do
-  STATE=$(tracker "$s" story | jq -r '.current_state')
+  STATE=$(tracker story "$s" | jq -r '.current_state')
   if [[ $STATE = 'accepted' ]]; then
     BRANCH=$(git ls-remote --heads 2>&1 |grep "$s")
     echo "$STATE $s $BRANCH";
