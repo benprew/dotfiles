b690ba2007d2efa9674d1367f886dc046aa25789
Ben Prew <ben@throwingbones.com>
2018-11-20 14:04:49 -0800

    add dumb-jump. disable electric modes.

--- core/init.el
+++ core/init.el
@@ -1,19 +1,26 @@
-(prelude-require-packages '(solarized-theme dash-at-point web-mode helm-w3m jq-mode fish-mode graphviz-dot-mode))
+(prelude-require-packages '(solarized-theme dash-at-point web-mode helm-w3m jq-mode fish-mode graphviz-dot-mode dumb-jump))
+
+
+(dumb-jump-mode)
+
+(setq projectile-project-search-path '("~/src/"))
+(projectile-cleanup-known-projects)
 
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
 (setq browse-url-browser-function 'w3m-browse-url)
 (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+(setq w3m-use-cookies t)
 ;; optional keyboard short-cut
 ;;(global-set-key "\C-xm" 'browse-url-at-point)
-(setq w3m-use-cookies t)
 
 (require 'prelude-helm-everywhere)
 (require 'prelude-org)
 (require 'dash-at-point)
 
 (add-hook 'prog-mode-hook 'prelude-enable-whitespace)
+(add-hook 'prog-mode-hook (lambda () (smartparens-mode -1)))
 
 (setq global-flycheck-mode t)
 
@@ -26,3 +33,10 @@
 (autoload 'jq-mode "jq-mode.el"
     "Major mode for editing jq files" t)
 (add-to-list 'auto-mode-alist '("\\.jq$" . jq-mode))
+
+(abbrev-mode -1)
+(global-auto-composition-mode -1)
+(smartparens-global-mode -1)
+(smartparens-mode -1)
+(electric-pair-mode -1)
+(electric-quote-mode -1)
