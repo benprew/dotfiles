5a098f152b5b32e294671886d5ad472c9e351941
Ben Prew <ben@throwingbones.com>
2018-12-03 17:43:50 -0800

    add paths, check if they exist

--- ruby/init.fish
+++ ruby/init.fish
@@ -1,7 +1,11 @@
-set -x PATH /usr/local/opt/ruby/bin $PATH
+set -x PATH /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/2.5.0/bin/ $PATH
 # For compilers to find ruby you may need to set:
 set -x LDFLAGS "-L/usr/local/opt/ruby/lib"
 set -x CPPFLAGS "-I/usr/local/opt/ruby/include"
 
 # For pkg-config to find ruby you may need to set:
 set -x PKG_CONFIG_PATH "/usr/local/opt/ruby/lib/pkgconfig"
+
+if test -d ~/.rbenv
+    set -x PATH ~/.rbenv/shims $PATH
+end
