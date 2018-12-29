23dc30a9eec1d2c762c2da72d3dc69e0804ad094
Ben Prew <ben@throwingbones.com>
2018-12-28 22:10:17 -0800

    lua module

---
+++ lua/init.el
@@ -0,0 +1,10 @@
+(prelude-require-packages '(lua-mode))
+
+(add-to-list 'auto-mode-alist '("\\.script\\'" . lua-mode))
+(add-to-list 'auto-mode-alist '("\\.gui_script\\'" . lua-mode))
+
+(add-hook 'lua-mode-hook (lambda () (whitespace-mode -1)))
+(add-hook 'lua-mode-hook (lambda () (setq-local indent-tabs-mode t)))
+
+(setq-default lua-indent-level 4)
+(setq-default tab-width 4)
