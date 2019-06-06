a6a3324a5cc777e7937f012727a3961ca9dfa539
Ben Prew <ben@throwingbones.com>
2019-06-06 13:21:43 -0700

    add linliveanalytics1 custom functions

--- comscore/init.sh
+++ comscore/init.sh
@@ -1,2 +1,17 @@
 # use git 2.9 on linliveanalytics
 source /opt/rh/rh-git29/enable
+
+if [ -f /home/as_worker/as_work/analytical-solutions/etc/bashrc ]; then
+    . /home/as_worker/as_work/analytical-solutions/etc/bashrc
+fi
+
+daterange () {
+    startdate=$1
+    enddate=$2
+
+    curr="$startdate"
+    while [[ ! "$curr" > "$enddate" ]]; do
+        echo "$curr"
+        curr=$( date +%Y-%m-%d --date "$curr +1 day" )
+    done
+}
