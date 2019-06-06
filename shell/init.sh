f1c07ddbf136898c53328626114022d4fcf200c3
Ben Prew <ben@throwingbones.com>
2019-06-06 13:23:24 -0700

    remove dotfile status from PROMPT

--- shell/init.sh
+++ shell/init.sh
@@ -1,1 +1,1 @@
-export PS1=export PS1='\033]0;${USER}@${HOSTNAME}: ${PWD}\007 $(__dotfiles_status)$(__git_ps1 "(%s) ")\[\e[0;32m\]\u@\h\[\e[m\] \w\n\$ '
+export PS1=export PS1='\033]0;${USER}@${HOSTNAME}: ${PWD}\007$(__git_ps1 "(%s) ")\[\e[0;32m\]\u@\h\[\e[m\] \w\n\$ '
