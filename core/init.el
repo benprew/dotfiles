9c14cf01bb69d7381e5302d3aa408191630c681b
Ben Prew <ben@throwingbones.com>
2020-02-19 22:26:18 -0800

    use vhdl for .hdl files (nand2tetris)

--- core/init.el
+++ core/init.el
@@ -42,6 +42,10 @@
     "Major mode for editing jq files" t)
 (add-to-list 'auto-mode-alist '("\\.jq$" . jq-mode))
 
+;; for nand2tetris
+(add-to-list 'auto-mode-alist '("\\.hdl$" . vhdl-mode))
+(add-hook 'asm-mode-hook (lambda () (whitespace-mode -1)))
+
 (abbrev-mode -1)
 (global-auto-composition-mode -1)
 (smartparens-global-mode -1)
