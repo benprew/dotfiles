b03ce66aab0415931ae98760b0bb5b2377986cf2
Ben Prew <ben@throwingbones.com>
2020-02-19 22:15:09 -0800

    Use set -g to prevent duplicate paths from getting added
    See https://github.com/fish-shell/fish-shell/issues/5117 for details and recommendation

--- ruby/init.fish
+++ ruby/init.fish
@@ -1,5 +1,11 @@
 if test -d ~/.rbenv
-    set -x PATH ~/.rbenv/shims $PATH
+    set -g fish_user_paths $HOME/.rbenv/bin $HOME/.rbenv/shims $fish_user_paths
 end
 
-set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+if test -d ~/.gem/ruby/2.5.0/bin/
+    set -g fish_user_paths ~/.gem/ruby/2.5.0/bin/ $fish_user_paths
+end
+
+if test -d ~/.gem/ruby/2.7.0/bin/
+    set -g fish_user_paths ~/.gem/ruby/2.7.0/bin/ $fish_user_paths
+end
