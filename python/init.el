c10f6bc1b60099a8c8cac5df71b34a768ea5e819
Ben Prew <ben@throwingbones.com>
2020-04-28 21:06:45 -0700

    Use python3 executables by default.

--- python/init.el
+++ python/init.el
@@ -1,5 +1,5 @@
 (require 'prelude-python)
 (prelude-require-packages '(blacken))
 (add-hook 'python-mode-hook 'blacken-mode)
-
-(setq flycheck-python-flake8-executable "flake8")
+(setq flycheck-python-pycompile-executable "python3")
+(setq flycheck-python-flake8-executable "python3")
---
+++ python/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip3 install --user black
+pip3 install --user flake8
