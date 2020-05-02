8802856ef5430d13af8542188ef0112da41bdf13
Ben Prew <ben@throwingbones.com>
2020-05-02 12:51:34 -0700

    Add local gem bin dirs to path

--- ruby/init.fish
+++ ruby/init.fish
@@ -2,10 +2,9 @@ if test -d ~/.rbenv
     set -g fish_user_paths $HOME/.rbenv/bin $HOME/.rbenv/shims $fish_user_paths
 end
 
-if test -d ~/.gem/ruby/2.5.0/bin/
-    set -g fish_user_paths ~/.gem/ruby/2.5.0/bin/ $fish_user_paths
-end
-
-if test -d ~/.gem/ruby/2.7.0/bin/
-    set -g fish_user_paths ~/.gem/ruby/2.7.0/bin/ $fish_user_paths
+for f in ~/.gem/ruby/* do
+    set bin_dir $f/bin/
+    if test -d $bin_dir
+        set -g fish_user_paths $bin_dir $fish_user_paths
+    end
 end
