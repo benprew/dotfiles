14073578515a6325dac8a80c7b8ca647720691bc
Ben Prew <ben@throwingbones.com>
2020-11-23 13:29:41 -0800

    local laptop Comscore config

---
+++ comscore/init.fish
@@ -0,0 +1,5 @@
+export ROOT_WD=$HOME/src/analytical-solutions-trees/master
+export AS_REDSHIFT_URI=postgresql://as-linear.db.csas.csa.comscore.com:5439/live
+export AS_LINEAR_DMAP_IAM_ROLE=arn:aws:iam::822154877347:role/r-csas-as-linear-user,arn:aws:iam::354890984934:role/r-csps-csdmap-s3-user
+
+set -g fish_user_paths $fish_user_paths $ROOT_WD/bin
