657f056af52cbec3951a1dd0d69974a2031095f7
Ben Prew <ben@throwingbones.com>
2021-01-22 22:04:11 -0800

    move web-mode into javascript module

--- javascript/init.el
+++ javascript/init.el
@@ -2,3 +2,15 @@
 (require 'prelude-web)
 
 (setq js2-basic-offset 2)
+(local-set-key (kbd "RET") 'newline-and-indent)
+
+(use-package web-mode
+  :mode ("\\.phtml\\'" "\\.tpl\\.php\\'" "\\.blade\\.php\\'"
+         "\\.jsp\\'"  "\\.as[cp]x\\'"  "\\.erb\\'"  "\\.html?\\'"
+         "/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'")
+  :init  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-disable-autocompletion t)
+  ; Disable whitespace-mode when using web-mode
+  (add-hook 'web-mode-hook (lambda () (whitespace-mode -1))))
