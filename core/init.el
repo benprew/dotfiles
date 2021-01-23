ac63ae36c1cdb206734fbf0a9497aa1d5e124a03
Ben Prew <ben@throwingbones.com>
2021-01-22 22:03:49 -0800

    Switch from prelude-require-packages to use-package

--- core/init.el
+++ core/init.el
@@ -1,8 +1,24 @@
-(prelude-require-packages '(web-mode jq-mode fish-mode graphviz-dot-mode dumb-jump helm-ag))
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
 
-;; dump-jump now integrates with xref-backend, so you can use M-. and M-, to search and return
-(dumb-jump-mode)
-(add-to-list 'xref-backend-functions #'dumb-jump-xref-activate)
+(use-package graphviz-dot-mode
+  :ensure t)
+
+(use-package jq-mode
+  :ensure t
+  :mode "\\.jq'")
+
+(use-package fish-mode
+  :ensure t
+  :mode "\\.fish'")
+
+(use-package dumb-jump
+  :ensure t
+  :init (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
+
 
 (setq projectile-project-search-path '("~/src/"))
 (projectile-cleanup-known-projects)
@@ -11,8 +27,6 @@
 (setq mac-command-modifier 'meta)
 
 (require 'prelude-helm-everywhere)
-(require 'prelude-org)
-(require 'dash-at-point)
 
 (add-hook 'prog-mode-hook 'prelude-enable-whitespace)
 (add-hook 'prog-mode-hook (lambda () (smartparens-mode -1)))
@@ -21,20 +35,11 @@
 
 (setq global-flycheck-mode t)
 
-(windmove-default-keybindings 'meta)
-
-(global-set-key (kbd "<ESC> <left>")  'windmove-left)
-(global-set-key (kbd "<ESC> <right>") 'windmove-right)
-(global-set-key (kbd "<ESC> <up>")    'windmove-up)
-(global-set-key (kbd "<ESC> <down>")  'windmove-down)
+(windmove-default-keybindings 'shift)
 
 (setq confirm-kill-emacs 'yes-or-no-p)
 
 (setq initial-scratch-message ";Don't ignore your dreams\n;Don't work too much\n;Say what you think\n;Cultivate friendships\n;Be happy.\n\n")
-
-(autoload 'jq-mode "jq-mode.el"
-    "Major mode for editing jq files" t)
-(add-to-list 'auto-mode-alist '("\\.jq$" . jq-mode))
 
 (abbrev-mode -1)
 (global-auto-composition-mode -1)
@@ -46,6 +51,5 @@
 (setq undo-tree-auto-save-history nil)
 (if (display-graphic-p)
     (setq browse-url-browser-function 'browse-url-default-browser))
-
 
 (server-start)
