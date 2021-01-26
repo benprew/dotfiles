96161b4a35f27dea13d9ab209fa69f17564365f4
Ben Prew <ben@throwingbones.com>
2021-01-26 00:47:53 -0800

    update pyenv init

--- python/init.sh
+++ python/init.sh
@@ -1,1 +1,9 @@
 export PATH=$PATH:~/.local/bin
+
+if command -v pyenv >/dev/null; then
+    export PATH=$(pyenv root)/shims:$PATH
+fi
+
+# if command -v pyenv-virtualenv-init > /dev/null; then
+#     eval "$(pyenv virtualenv-init -)";
+# fi
