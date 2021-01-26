e4ef0ee06d7de1d19ab4c5cf57f0f886065360c2
Ben Prew <ben@throwingbones.com>
2021-01-26 00:49:14 -0800

    comscore updates

--- comscore/init.el
+++ comscore/init.el
@@ -1,3 +1,9 @@
-(prelude-require-packages '(crontab-mode))
+(prelude-require-packages '(crontab-mode ansible ansible-doc ansible-vault))
+
+;; https://github.com/k1LoW/emacs-ansible
+(add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
+
+;; https://github.com/emacsorphanage/ansible-doc
+(add-hook 'yaml-mode-hook #'ansible-doc-mode)
 
 (add-to-list 'auto-mode-alist '("\\.cron$" . crontab-mode))
--- comscore/init.fish
+++ comscore/init.fish
@@ -1,5 +1,6 @@
 export ROOT_WD=$HOME/src/analytical-solutions-trees/master
 export AS_REDSHIFT_URI=postgresql://as-linear.db.csas.csa.comscore.com:5439/live
 export AS_LINEAR_DMAP_IAM_ROLE=arn:aws:iam::822154877347:role/r-csas-as-linear-user,arn:aws:iam::354890984934:role/r-csps-csdmap-s3-user
+export AS_LINEAR_IAM_ROLE=arn:aws:iam::822154877347:role/r-csas-as-linear-user,arn:aws:iam::809055356645:role/r-cssm-redshift-user
 
 set -g fish_user_paths $fish_user_paths $ROOT_WD/bin
