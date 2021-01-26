75a66e47fa6f98852a051a4cbffd75982449b3b8
Ben Prew <ben@throwingbones.com>
2021-01-26 00:47:32 -0800

    add jinja2-mode to python

--- python/init.el
+++ python/init.el
@@ -1,5 +1,9 @@
 (require 'prelude-python)
-(prelude-require-packages '(blacken))
+(prelude-require-packages '(blacken jinja2-mode))
 (add-hook 'python-mode-hook 'blacken-mode)
 (setq flycheck-python-pycompile-executable "python3")
 (setq flycheck-python-flake8-executable "python3")
+
+(autoload 'jinja2-mode "jinja2-mode.el"
+  "Major mode for editing jq files" t)
+(add-to-list 'auto-mode-alist '("\\.jt$" . jinja2-mode))
