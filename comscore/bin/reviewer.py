bde9ad3be28ebd0574a067251b3334db8f0274df
Ben Prew <ben@throwingbones.com>
2021-01-28 21:52:32 -0800

    use list comprehension instead of filter/lambda

--- comscore/bin/reviewer.py
+++ comscore/bin/reviewer.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 
+as_dev = ["mfoster", "rchurch", "bprew", "cgoughnour", "mgenova"]
+
 
 def reviewer(pr_owner, pr):
-    reviewers = list(
-        filter(
-            lambda x: x != pr_owner,
-            ["mfoster", "rchurch", "bprew", "cgoughnour", "mgenova"],
-        )
-    )
+    reviewers = [x for x in as_dev if x != pr_owner]
     return reviewers[pr % len(reviewers)]
 
 
