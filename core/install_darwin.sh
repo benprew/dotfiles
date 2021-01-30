620c47dc2977a386f917ad342b23b1efbcdca116
Ben Prew <ben@throwingbones.com>
2021-01-29 17:43:53 -0800

    prelude is installed in install.sh, no need to install it again

--- core/install_darwin.sh
+++ core/install_darwin.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ ! -f ~/.emacs.d/.projectile ]]; then
-    curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
-fi
-
 # install fish
 if ! grep -q fish /etc/shells; then
    echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
