9cb54d27f421fb06150616e76add850d8add4bbd
Ben Prew <ben@throwingbones.com>
2021-01-30 22:54:36 -0800

    check if pyenv is available before running init

--- python/init.fish
+++ python/init.fish
@@ -1,2 +1,2 @@
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
+status --is-interactive; and command -v pyenv; and pyenv init - | source
+status --is-interactive; and command -v pyenv; and pyenv virtualenv-init - | source
