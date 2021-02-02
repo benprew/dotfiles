01a1db084c0dd60e0236beb721ef04495be75e7c
Ben Prew <ben@throwingbones.com>
2021-02-01 23:20:04 -0800

    Add --user ruby paths to bin

--- ruby/init.fish
+++ ruby/init.fish
@@ -1,9 +1,9 @@
-# for f in ~/.gem/ruby/* do
-#     set bin_dir $f/bin/
-#     if test -d $bin_dir
-#         set -g fish_user_paths $bin_dir $fish_user_paths
-#     end
-# end
+for f in ~/.gem/ruby/* do
+    set bin_dir $f/bin/
+    if test -d $bin_dir
+        set -g fish_user_paths $bin_dir $fish_user_paths
+    end
+end
 
 if test -d ~/.rbenv
     set -g fish_user_paths $HOME/.rbenv/bin $HOME/.rbenv/shims $fish_user_paths
