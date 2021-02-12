b99f8f955bfc8058d44ad1b6e4786c8f6863187a
Ben Prew <ben@throwingbones.com>
2021-02-11 16:12:49 -0800

    Make 'command' quiet

--- python/init.fish
+++ python/init.fish
@@ -1,2 +1,2 @@
-status --is-interactive; and command -v pyenv; and pyenv init - | source
-status --is-interactive; and command -v pyenv; and pyenv virtualenv-init - | source
+status --is-interactive; and command -vq pyenv; and pyenv init - | source
+status --is-interactive; and command -vq pyenv; and pyenv virtualenv-init - | source
--- python/init.sh
+++ python/init.sh
@@ -1,9 +1,9 @@
 export PATH=$PATH:~/.local/bin
 
-if command -v pyenv >/dev/null; then
+if command -vq pyenv >/dev/null; then
     export PATH=$(pyenv root)/shims:$PATH
 fi
 
-# if command -v pyenv-virtualenv-init > /dev/null; then
+# if command -vq pyenv-virtualenv-init > /dev/null; then
 #     eval "$(pyenv virtualenv-init -)";
 # fi
