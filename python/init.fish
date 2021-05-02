e2fca6d87d0568952e5a7250f8c38aa16296d2fe
Ben Prew <ben@throwingbones.com>
2021-05-02 15:09:26 -0700

    add pyenv/bin to path if pyenv/bin exists

--- python/init.fish
+++ python/init.fish
@@ -1,2 +1,5 @@
+if test -e $HOME/.pyenv/bin
+    set -g fish_user_paths $fish_user_paths $HOME/.pyenv/bin
+end
 status --is-interactive; and command -vq pyenv; and pyenv init - | source
 status --is-interactive; and command -vq pyenv; and pyenv virtualenv-init - | source
