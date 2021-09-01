f3c0e98534ab8e61838343ef4c2249b03dea91fb
Ben Prew <ben@throwingbones.com>
2021-08-31 18:52:44 -0700

    update comscore helpers

--- comscore/bin/bulk_delete.py
+++ comscore/bin/bulk_delete.py
@@ -6,16 +6,24 @@ import boto3
 
 s3 = boto3.client("s3")
 lines = []
+bucket = sys.argv[1]
 
-with open(sys.argv[1], "r") as fh:
+with open(sys.argv[2], "r") as fh:
     lines = fh.readlines()
+
+print(f"Deleting {len(lines)} objects")
 
 for i in range(0, len(lines), 1000):
     s = i
     e = min(i + 1000, len(lines))
     print(s, e)
     objs = list(map(lambda x: {"Key": x.rstrip()}, lines[s:e]))
-    s3.delete_objects(
-        Bucket="csps-csdmap-as-data",
+    resp = s3.delete_objects(
+        Bucket=bucket,
         Delete={"Objects": objs},
     )
+
+    if "Deleted" not in resp:
+        raise Exception(f"Error deleting files {resp}")
+
+    print(f"Deleted {len(resp['Deleted'])} items")
--- comscore/bin/read_parquet.py
+++ comscore/bin/read_parquet.py
@@ -2,10 +2,44 @@
 
 import sys
 
-import pandas as pd
+
+def vaex_read(fn):
+    import vaex
+
+    df = vaex.open(fn)
+    print(df.schema())
+    print(df)
+
+
+def pandas_read(fn):
+    import pandas as pd
+
+    df = pd.read_parquet(fn, engine="pyarrow")
+
+    # print(df.columns)
+    pd.set_option("display.max_columns", None)
+    print(df.info(verbose=True))
+    print(df.head(3))
+
+
+def pyarrow_read(source):
+    from pyarrow.parquet import ParquetFile
+    import pprint
+
+    # Source is either the filename or an Arrow file handle (which could be on HDFS)
+
+    # TODO: figure out how to read from s3 directly
+    args = {}
+    if "s3://" in source:
+        args["filesystem"] = "s3"
+
+    m = ParquetFile(source, pre_buffer=True).metadata
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(m)
+
 
 fn = sys.argv[1]
 
-df = pd.read_parquet(fn, engine="pyarrow")
-
-print(df.columns)
+# vaex_read(fn)
+# pyarrow_read(fn)
+pandas_read(fn)
