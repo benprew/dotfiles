03fbecab74a70024eaa7ff6bc2d73f3d1357b029
Ben Prew <ben@throwingbones.com>
2021-10-04 18:10:01 -0700

    use lsp-mode instead of eglot

--- core/init.el
+++ core/init.el
@@ -104,7 +104,39 @@
 (use-package emacs-lisp-mode
   :mode "\\.el\\.symlink\\'")
 
-(use-package eglot
+;; (use-package eglot
+;;   :ensure t
+;;   :defer 2
+;;   :hook (python-mode . eglot-ensure))
+
+
+;;; LSP MODE
+
+(use-package lsp-mode
   :ensure t
-  :defer 2
-  :hook (python-mode . eglot-ensure))
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (ruby-mode . lsp)
+         (python-mode . lsp)
+         (sh-mode . lsp)
+         (go-mode . lsp)
+         (web-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; if you are helm user
+(use-package helm-lsp
+  :ensure t
+  :commands helm-lsp-workspace-symbol)
+
+;; optional if you want which-key integration
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
--- go/init.el
+++ go/init.el
@@ -1,10 +1,18 @@
 (require 'use-package)
-
 (require 'prelude-go)
-
-(use-package prelude-go
-  :defer 2)
+(require 'lsp-mode)
 
 (use-package go-playground
   :ensure t
   :defer 3)
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+(lsp-register-custom-settings
+ '(("gopls.completeUnimported" t t)
+   ("gopls.staticcheck" t t)))
