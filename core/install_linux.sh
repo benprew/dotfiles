94a26881182252c54bf3d358a1565b7764c3da57
Ben Prew <ben@throwingbones.com>
2021-10-08 00:01:19 -0700

    Add redshift to core packages

--- core/install_linux.sh
+++ core/install_linux.sh
@@ -3,3 +3,6 @@
 if ! fc-list | grep -i sourcecodepro &>/dev/null; then
     ./install-source-code-pro-ubuntu.sh
 fi
+
+echo "starting redshift night-light in background, it should show up in your taskbar"
+redshift-gtk &
