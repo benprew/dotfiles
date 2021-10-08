6696bc52e97cdccc1544c5228edfbd6c94e1482c
Ben Prew <ben@throwingbones.com>
2021-10-07 23:41:04 -0700

    Use solargraph with bundle exec and format buffer on save

--- ruby/init.el
+++ ruby/init.el
@@ -1,2 +1,8 @@
 (require 'prelude-ruby)
 (require 'prelude-web)
+
+(setq lsp-solargraph-use-bundler 't)
+
+(defun lsp-ruby-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t))
+(add-hook 'ruby-mode-hook #'lsp-ruby-install-save-hooks)
