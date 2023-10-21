a524cfbf1293f493b2dcf0df16b7bad1e1b8614f
Ben Prew <ben@throwingbones.com>
2023-10-20 20:53:59 -0700

    [shell] sync changes

---
+++ shell/init.el
@@ -0,0 +1,11 @@
+(use-package flymake-shellcheck
+  :ensure t
+  :commands flymake-shellcheck-load
+  :hook
+  ('sh-mode . 'flymake-shellcheck-load)
+  ('sh-mode . 'flymake-mode))
+
+(use-package fish-mode
+  :defer 3
+  :ensure t
+  :mode ("\\.fish\\'" "\\.fish\\.symlink\\'"))
