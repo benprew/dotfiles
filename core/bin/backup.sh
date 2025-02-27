691a122fb3a979e0596a42daeb772a72d4785c5b
Ben Prew <ben@throwingbones.com>
2025-02-26 22:46:48 -0800

    Instead of excluding each directory, only backup specific directories

--- core/bin/backup.sh
+++ core/bin/backup.sh
@@ -6,19 +6,17 @@ HOSTNAME=$(hostname)
 export PATH=$PATH:/usr/bin # restic lives in /usr/bin on fedora
 
 export RESTIC_REPOSITORY="sftp:ben@nas.local:/mnt/nas/backup/$HOSTNAME"
-export RESTIC_PASSWORD_FILE="$HOME/.restic_pass"
+export RESTIC_PASSWORD_FILE="$HOME/secrets/.restic_pass"
 
-restic backup "$HOME" --verbose \
-       -e "$HOME/.wine" \
-       -e "$HOME/restore" \
-       -e "$HOME/.local" \
-       -e "$HOME/.cache" \
-       -e "$HOME/Games" \
-       -e "$HOME/.var" \
-       -e "$HOME/.cargo" \
-       -e "$HOME/.mozilla" \
-       -e "$HOME/.log" \
-       -e "$HOME/.npm"
+restic backup "$HOME/src" --verbose \
+       -e "**/_build" \
+       -e "**/target"
+
+restic backup "$HOME/Downloads" --verbose
+restic backup "$HOME/Documents" --verbose
+restic backup "$HOME/.ssh" --verbose
+restic backup "$HOME/secrets" --verbose
+
 # cleanup old snapshots
 # restic forget --keep-weekly 10
 # verify backup
