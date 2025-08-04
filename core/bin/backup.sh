e4814ddebe00d5ed2f19c5df8194ae7d12451363
Ben Prew <ben@throwingbones.com>
2025-08-03 19:41:54 -0700

    [core] each call to restic creates a snapshot, so backup in single call

--- core/bin/backup.sh
+++ core/bin/backup.sh
@@ -8,14 +8,16 @@ export PATH=$PATH:/usr/bin # restic lives in /usr/bin on fedora
 export RESTIC_REPOSITORY="sftp:ben@nas.local:/mnt/nas/backup/$HOSTNAME"
 export RESTIC_PASSWORD_FILE="$HOME/secrets/.restic_pass"
 
-restic backup "$HOME/src" --verbose \
-       -e "**/_build" \
-       -e "**/target"
-
-restic backup "$HOME/Downloads" --verbose
-restic backup "$HOME/Documents" --verbose
-restic backup "$HOME/.ssh" --verbose
-restic backup "$HOME/secrets" --verbose
+restic backup "$HOME/src" \
+	"$HOME/Downloads" \
+	"$HOME/Documents" \
+	"$HOME/.ssh" \
+	"$HOME/secrets" \
+	"$HOME/Books" \
+	"$HOME/Music" \
+	-e "**/_build" \
+	-e "**/target" \
+	--verbose
 
 # cleanup old snapshots
 # restic forget --keep-weekly 10
