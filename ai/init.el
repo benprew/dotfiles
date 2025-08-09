b2e4fe56d71c0d8d15dd0d5504d2973369508b41
Ben Prew <ben@throwingbones.com>
2025-08-08 18:00:40 -0700

    Add openai key, move setq's to custom

--- ai/init.el
+++ ai/init.el
@@ -45,23 +45,28 @@
   :ensure t
   :vc (:url "https://github.com/MatthewZMD/aidermacs" :branch "master")
   :config
-  (setq aidermacs-default-chat-mode 'architect)
-  (setq aidermacs-use-architect-mode t) ;; this is deprecated but still needed for now
-  (setq aidermacs-default-model "gemini/gemini-2.5-flash-lite-preview-06-17")
-  (setq aidermacs-architect-model "gemini/gemini-2.5-flash-lite-preview-06-17")
-  (setq aidermacs-editor-model "anthropic/claude-4-sonnet-20250514")
   (setenv "ANTHROPIC_API_KEY"
           (with-temp-buffer
             (insert-file-contents (expand-file-name "~/secrets/claude.ai.key"))
+            (string-trim (buffer-string))))
+  (setenv "OPENAI_API_KEY"
+          (with-temp-buffer
+            (insert-file-contents (expand-file-name "~/secrets/open.ai.key"))
             (string-trim (buffer-string))))
   (setenv "GEMINI_API_KEY"
           (with-temp-buffer
             (insert-file-contents (expand-file-name "~/secrets/gemini.key"))
             (string-trim (buffer-string))))
   (setenv "AIDER_YES_ALWAYS" "YES")
-  (global-set-key (kbd "C-c a") 'aidermacs-transient-menu))
-;; Change multiline input key (default is S-<return>)
-(setq aidermacs-comint-multiline-newline-key "C-<return>")
+  (global-set-key (kbd "C-c a") 'aidermacs-transient-menu)
+  :custom
+  ;; Change multiline input key (default is S-<return>)
+  (aidermacs-comint-multiline-newline-key "C-<return>")
+  (aidermacs-default-chat-mode 'architect)
+  (aidermacs-use-architect-mode t) ;; this is deprecated but still needed for now
+  (aidermacs-default-model "gemini/gemini-2.5-flash-lite-preview-06-17")
+  (aidermacs-architect-model "gemini/gemini-2.5-flash-lite-preview-06-17")
+  (aidermacs-editor-model "anthropic/claude-4-sonnet-20250514"))
 
 
 (with-eval-after-load 'project
