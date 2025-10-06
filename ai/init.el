a93139e3620984f5905b5373cccd36f065fcd63c
Ben Prew <ben@throwingbones.com>
2025-10-05 22:24:20 -0700

    [ai] remove minuet
    It seems like it's usually more trouble than it's worth. That and I do a lot with
    Claude Code now, and having context and prompt is more important than "smart autocomplete"

--- ai/init.el
+++ ai/init.el
@@ -10,37 +10,6 @@
                           (string-trim (buffer-string)))
                    :stream t)))
 
-(use-package minuet
-  :ensure t
-  :bind
-  (("M-y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
-   ("M-i" . #'minuet-show-suggestion) ;; use overlay for completion
-   ("C-c m" . #'minuet-configure-provider)
-   :map minuet-active-mode-map
-   ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
-   ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
-   ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
-   ("M-A" . #'minuet-accept-suggestion) ;; accept whole completion
-   ;; Accept the first line of completion, or N lines with a numeric-prefix:
-   ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
-   ("M-a" . #'minuet-accept-suggestion-line)
-   ("M-e" . #'minuet-dismiss-suggestion))
-
-  :init
-  ;; if you want to enable auto suggestion.
-  ;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
-  (add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
-
-  :config
-  ;; You can use M-x minuet-configure-provider to interactively configure provider and model
-  (setq minuet-provider 'gemini)
-  (setenv "GEMINI_API_KEY"
-          (with-temp-buffer
-            (insert-file-contents (expand-file-name "~/secrets/gemini.key"))
-            (string-trim (buffer-string))))
-  (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 64))
-
-
 (use-package aidermacs
   :ensure t
   :vc (:url "https://github.com/MatthewZMD/aidermacs" :branch "master")
