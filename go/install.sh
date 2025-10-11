77426678e72d43cf4ace12a7c93b140719134955
Ben Prew <ben@throwingbones.com>
2025-10-11 14:17:44 -0700

    [go] install correct version of go based on CPU arch

--- go/install.sh
+++ go/install.sh
@@ -4,11 +4,29 @@ set -e
 
 GOVERSION="1.25.1"
 
+# Detect architecture
+ARCH=$(uname -m)
+case $ARCH in
+    x86_64)
+        GOARCH="amd64"
+        ;;
+    aarch64|arm64)
+        GOARCH="arm64"
+        ;;
+    armv7l|armv6l)
+        GOARCH="armv6l"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
 if ! (command -v go &>/dev/null && go version |grep $GOVERSION); then
-    echo "Installing go$GOVERSION"
+    echo "Installing go$GOVERSION for linux-$GOARCH"
     cd "$(mktemp -d)" || exit 1
-    wget "https://go.dev/dl/go$GOVERSION.linux-amd64.tar.gz"
-    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$GOVERSION.linux-amd64.tar.gz
+    wget "https://go.dev/dl/go$GOVERSION.linux-$GOARCH.tar.gz"
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$GOVERSION.linux-$GOARCH.tar.gz
 fi
 
 export PATH=$PATH:/usr/local/go/bin
