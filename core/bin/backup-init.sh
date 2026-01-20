9e32ffcdbcf16a2bf4245e3d2846b9a8840ce591
Ben Prew <ben@throwingbones.com>
2026-01-19 19:14:55 -0800

    [core] backup improvements
    - add init script to initialize restic repo
    - install restic package in apt/dnf
    - remove secrets dir from backup, secrets should not be duplicated

--- core/bin/backup-init.sh
+++ core/bin/backup-init.sh
@@ -8,15 +8,7 @@ export PATH=$PATH:/usr/bin # restic lives in /usr/bin on fedora
 export RESTIC_REPOSITORY="sftp:ben@nas.local:/mnt/nas/backup/$HOSTNAME"
 export RESTIC_PASSWORD_FILE="$HOME/secrets/.restic_pass"
 
-restic backup "$HOME/src" \
-    "$HOME/Downloads" \
-    "$HOME/Documents" \
-    "$HOME/.ssh" \
-    "$HOME/Books" \
-    "$HOME/Music" \
-    -e "**/_build" \
-    -e "**/target" \
-    --verbose
+restic init --repo "$RESTIC_REPOSITORY" --verbose
 
 # cleanup old snapshots
 # restic forget --keep-weekly 10
