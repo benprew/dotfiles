3f14b95f3cc6be2182963cb6c3338fde134391d0
Ben Prew <ben@throwingbones.com>
2026-05-27 15:22:37 -0400

    [core] fix bug in powertop installation script

--- core/install-powertop.sh
+++ core/install-powertop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ -e /etc/systemd/system/powertop.service ]; then
    echo "Powertop already installed"
    exit 0
@@ -7,5 +9,5 @@ fi
 
 SYSD_DIR=/etc/systemd/system/
 mkdir -p "$SYSD_DIR"
-sudo cp powertop.service "$SYSD_DIR"
+sudo cp "$SCRIPT_DIR/powertop.service" "$SYSD_DIR"
 sudo systemctl enable powertop.service
