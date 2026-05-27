0716be2b3180b70773f4239cbad2369247e8bc67
Ben Prew <ben@throwingbones.com>
2026-05-27 11:23:30 -0400

    [python] add uv install dir to PATH

--- python/init.fish
+++ python/init.fish
@@ -2,3 +2,6 @@ add_path $HOME/.pyenv/bin
 add_path (python3 -m site --user-base)/bin
 status --is-interactive; and command -vq pyenv; and pyenv init - | source
 status --is-interactive; and command -vq pyenv; and pyenv virtualenv-init - | source
+
+# uv
+fish_add_path "/Users/ben/.local/bin"
