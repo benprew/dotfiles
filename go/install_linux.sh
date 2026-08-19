7b08ac1da3dc12a49d47772fd337eca685ed44f2
Ben Prew <ben@throwingbones.com>
2026-08-18 21:28:20 -0700

    go: move to system packages for Fedora and Mac

--- go/install_linux.sh
+++ go/install_linux.sh
@@ -1,36 +1,4 @@
 #!/bin/bash
 
-set -e
-
-GOVERSION="1.26.4"
-
-# Detect architecture
-ARCH=$(uname -m)
-case $ARCH in
-    x86_64)
-        GOARCH="amd64"
-        ;;
-    aarch64|arm64)
-        GOARCH="arm64"
-        ;;
-    armv7l|armv6l)
-        GOARCH="armv6l"
-        ;;
-    *)
-        echo "Unsupported architecture: $ARCH"
-        exit 1
-        ;;
-esac
-
-if ! (command -v go &>/dev/null && go version |grep $GOVERSION); then
-    echo "Installing go$GOVERSION for linux-$GOARCH"
-    cd "$(mktemp -d)" || exit 1
-    wget "https://go.dev/dl/go$GOVERSION.linux-$GOARCH.tar.gz"
-    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$GOVERSION.linux-$GOARCH.tar.gz
-fi
-
-export PATH=$PATH:/usr/local/go/bin
-
-echo "Installing gopls"
-go install golang.org/x/tools/gopls@latest
+echo "Installing modernize"
 go install golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest
