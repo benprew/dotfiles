95aadb2bfa52c9d92a38af308c3a5a19a3c91a82
Ben Prew <ben@throwingbones.com>
2026-09-14 11:19:00 -0700

    git: Fix line limit it magit commit buffers.

--- git/init.el
+++ git/init.el
@@ -1,6 +1,7 @@
 (use-package magit
   :ensure t
-  :commands (magit-status magit-project-status))
+  :commands (magit-status magit-project-status)
+  :hook (git-commit-setup . (lambda () (setq fill-column 72))))
 
 (use-package pinentry
   :ensure t
